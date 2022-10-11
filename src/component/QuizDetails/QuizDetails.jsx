4a966641318731bb90ed3f802d6b0fe9bab44296
Abdur Rahman Emon <aremon86851@gmail.com>
2022-10-11 12:47:12 +0600

    Quiz tostify added

--- src/component/QuizDetails/QuizDetails.jsx
+++ src/component/QuizDetails/QuizDetails.jsx
@@ -1,19 +1,54 @@
 import React from 'react';
 import QuizOptions from '../QuizOptions/QuizOptions';
+import { EyeIcon } from '@heroicons/react/24/solid'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const QuizDetails = ({ quiz }) => {
     // console.log(quiz)
     const { question, options, correctAnswer, id } = quiz
     const handleToCorrect = answer => {
         if (correctAnswer === answer) {
-            console.log("Right Answer")
+            toast('Your answer is Correct', {
+                position: "top-center",
+                autoClose: 1000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+            });
         } else {
-            console.log("Wrong Answer")
+            toast('Your answer is Wrong', {
+                position: "top-center",
+                autoClose: 1000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+            });
         }
     }
+    const notify = () => toast('Correct Answer is' + ' ' + '"' + correctAnswer + '"', {
+        position: "top-right",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+    });
     return (
-        <div className='bg-slate-100 my-4 p-10  '>
-            <p><span className='font-bold'>Quiz :- </span>{question}.</p>
+        <div className='bg-slate-100 my-4 p-10  rounded-lg'>
+            <div className='flex justify-between items-center'>
+                <p><span className='font-bold'>Quiz :- </span>{question}.</p> <EyeIcon className='w-6 h-6 cursor-pointer' onClick={notify} />
+                <ToastContainer />
+            </div>
             {
                 options.map((option, idx) => <QuizOptions key={idx} option={option} handleToCorrect={handleToCorrect} />)
             }
